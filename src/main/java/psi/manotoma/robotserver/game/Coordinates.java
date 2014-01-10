a8d39db8c1e15e77c444c2ad5f71869b350a1f07
Tomas Mano <tomasmano@gmail.com>
2014-01-10 10:42:17 -0500

    Add equals and hashcode to Coordinates

--- src/main/java/psi/manotoma/robotserver/game/Coordinates.java
+++ src/main/java/psi/manotoma/robotserver/game/Coordinates.java
@@ -52,6 +52,32 @@ public class Coordinates {
         return new Coordinates(gen.nextInt(MIN.size() - 1, MAX.size() - 1), 
                                gen.nextInt(MIN.size() - 1, MAX.size() - 1));
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Coordinates other = (Coordinates) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 89 * hash + this.x;
+        hash = 89 * hash + this.y;
+        return hash;
+    }
     
     public String print(){
         return "(" + x + "," + y + ")";
