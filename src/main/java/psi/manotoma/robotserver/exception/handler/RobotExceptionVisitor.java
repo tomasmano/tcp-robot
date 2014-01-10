0659082c09134a00cc78306762f37a46bdcab55c
Tomas Mano <tomasmano@gmail.com>
2014-01-10 12:09:34 -0500

    Add UnknowCommandException

--- src/main/java/psi/manotoma/robotserver/exception/handler/RobotExceptionVisitor.java
+++ src/main/java/psi/manotoma/robotserver/exception/handler/RobotExceptionVisitor.java
@@ -8,6 +8,7 @@ import psi.manotoma.robotserver.server.support.sender.RobotResponseSender;
 import java.io.OutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import psi.manotoma.robotserver.exception.UnknownCommandException;
 
 /**
  *
@@ -17,11 +18,15 @@ public class RobotExceptionVisitor {
 
     public static final Logger LOG = LoggerFactory.getLogger(RobotExceptionVisitor.class);
 
-    /**
-     * Handles code 400.
-     */
     public RobotResponse handle(BadSyntaxException ex, OutputStream os) {
         LOG.debug("Request had bad syntax, creating 500 response...");
+        RobotResponse res = RobotMsgsFactory.createErrorResponse(Status._500);
+        RobotResponseSender.getInstance().send(res, os);
+        return res;
+    }
+
+    public RobotResponse handle(UnknownCommandException ex, OutputStream os) {
+        LOG.debug("Request had unknown command [{}], creating 500 response...", ex.getMessage());
         RobotResponse res = RobotMsgsFactory.createErrorResponse(Status._500);
         RobotResponseSender.getInstance().send(res, os);
         return res;
