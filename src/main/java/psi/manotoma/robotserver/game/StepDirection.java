8f1b4e787bc281437b378a739aa2156683686c74
Tomas Mano <tomasmano@gmail.com>
2014-01-10 11:42:49 -0500

    Fix minor bug

--- src/main/java/psi/manotoma/robotserver/game/StepDirection.java
+++ src/main/java/psi/manotoma/robotserver/game/StepDirection.java
@@ -6,7 +6,7 @@ package psi.manotoma.robotserver.game;
  */
 public enum StepDirection {
     
-    N(0, 1), W(1, 0), S(0, -1), E(-1, 0);
+    N(0, 1), W(-1, 0), S(0, -1), E(1, 0);
 
     private int x;
     private int y;
